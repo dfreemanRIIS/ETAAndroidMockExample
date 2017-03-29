7a144974d5f59b32c3025cf3ef80b25e78a9318f
dfreemanRIIS <dfreeman@riis.com>
2017-03-29 10:29:37 -0400

    Create JsonParser

--- app/src/main/java/com/example/dfreeman/etaandroidmockexample/Model/JsonParser.java
+++ app/src/main/java/com/example/dfreeman/etaandroidmockexample/Model/JsonParser.java
@@ -1,8 +1,29 @@
 package com.example.dfreeman.etaandroidmockexample.Model;
 
-/**
- * Created by dfreeman on 3/29/17.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonParser {
+    public ArrayList<String> parseRoutes(String jsonString) throws JSONException{
+        //Create an array with the jsonString
+        final JSONObject obj = new JSONObject(jsonString);
+
+        //Create jsonArray for routeIDs
+        final JSONArray routeID = obj.getJSONArray("routeID");
+
+        //Get number of routes
+        final int n = routeID.length();
+
+        //for each route enter it into an array
+        ArrayList<String> returnArray = new ArrayList<String>();
+        for (int i = 0; i < n; ++i) {
+            final JSONObject instance = routeID.getJSONObject(i);
+            returnArray.add(instance.getString("routeID"));
+        }
+
+        return returnArray;
+    }
 }
