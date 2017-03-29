3087de646d2a0fa056fc5e5da935a9fe4b8ab19d
Brian Marshall <bmarshall@riis.com>
2017-03-29 13:11:34 -0400

    Added async task to routeActivity

--- app/src/main/java/com/example/dfreeman/etaandroidmockexample/View/RouteActivity.java
+++ app/src/main/java/com/example/dfreeman/etaandroidmockexample/View/RouteActivity.java
@@ -1,17 +1,24 @@
 package com.example.dfreeman.etaandroidmockexample.View;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.dfreeman.etaandroidmockexample.Controller.Controller;
 import com.example.dfreeman.etaandroidmockexample.R;
+
+import java.util.ArrayList;
 
 public class RouteActivity extends AppCompatActivity {
 
     public static final String EXTRA_COMPANY = "company";
     private int company;
-    private String[] routeNumbers;
+    private ArrayList<String> routeNumbers;
+    private Controller controller;
+    private String bla = "";
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,28 @@ public class RouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_route);
         Intent intent = getIntent();
         company = intent.getIntExtra(EXTRA_COMPANY, -1);
+        controller = new Controller();
+        test = (TextView) findViewById(R.id.test);
+        new AsyncCaller().execute();
+    }
+
+    private class AsyncCaller extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                bla = controller.fetchUrl(controller.getRoutesUrl(company));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            test.setText(bla);
+        }
+
     }
 }
