95e3859bbc1618d56a5f2edd6876a597c88ff09f
dfreemanRIIS <dfreeman@riis.com>
2017-03-29 10:31:20 -0400

    Wire parser to controller

--- app/src/main/java/com/example/dfreeman/etaandroidmockexample/Controller/Controller.java
+++ app/src/main/java/com/example/dfreeman/etaandroidmockexample/Controller/Controller.java
@@ -1,9 +1,19 @@
 package com.example.dfreeman.etaandroidmockexample.Controller;
 
 import com.example.dfreeman.etaandroidmockexample.Model.JsonFetcher;
+import com.example.dfreeman.etaandroidmockexample.Model.JsonParser;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 public class Controller {
     public String fetchUrl(String urlString) throws Exception {
         return JsonFetcher.fetchUrl(urlString);
+    }
+
+    public ArrayList<String> parseRoutes(String jsonString) throws JSONException {
+        JsonParser parser = new JsonParser();
+        return parser.parseRoutes(jsonString);
     }
 }
